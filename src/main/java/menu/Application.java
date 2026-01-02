7f3aada8a1144e91869ab89b6b61e05e02a12bfa
EHCTH <lees90896@gmail.com>
2026-01-02 11:37:10 +0900

    init

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,12 +1,6 @@
 package menu;
 
-import menu.bootstrap.AppConfig;
-import menu.interfaces.adapter.inbound.Controller;
-
 public class Application {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        Controller controller = appConfig.controller();
-        controller.run();
     }
 }
--- src/main/java/menu/application/port/inbound/MenuUseCase.java
+++
@@ -1,9 +0,0 @@
-package menu.application.port.inbound;
-
-import menu.domain.Name;
-
-import java.util.List;
-
-public interface MenuUseCase {
-    ResponseRow process(List<Name> names, List<List<String>> hateMenus);
-}
